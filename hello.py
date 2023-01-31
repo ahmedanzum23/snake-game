3f9ca1ea52f0164aa5736b8df2de49bfcbad1b45
ahmedanzum23 <ahmedanzum23@gmail.com>
2023-01-31 20:49:29 +0600

    second

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print('hello')
+#logical operations
+bol1= True
+bol2= False
+print('the result of booleans are ', bol1 and bol2)
+print('the result of booleans are ', bol1 or bol2)
+print('the result of booleans are ', not bol2)
